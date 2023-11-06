31c6c295840379dcc409ae694b255ea259c30b23
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2023-11-06 01:31:29 +0100

    changed to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
 import ProjectPages from "./projectPages/ProjectPages";
 
 function App() {
   return (
-    <Router>
+    <React.StrictMode>
+    <HashRouter >
       <div>
         <Routes>
           <Route index element={<Main />} />
           {ProjectPages()}
         </Routes>
       </div>
-    </Router>
+    </HashRouter >
+    </React.StrictMode>
   );
 }
 
